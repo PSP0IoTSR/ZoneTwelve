5663e4f257ffb36b838acb6d19e932df6acfd12f
ZoneTwelve <zonetwelve159@gmail.com>
2019-09-23 10:08:49 +0800

    [modify] exercise 2 - change pin 3 to pin 8

--- exercise2.ino
+++ exercise2.ino
@@ -1,10 +1,10 @@
 boolean led = false;
 void setup(){
   pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop(){
   digitalWrite(2, led^=true);
-  digitalWrite(3, !led);
+  digitalWrite(8, !led);
 }
